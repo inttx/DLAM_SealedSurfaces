added deeplabv3 model, validation loop, fixed loss calculation

src/dataset.py
@@ -6,11 +6,23 @@ from torch.utils.data import Dataset
 
 import rasterio
 from rasterio.windows import Window
+import numpy as np
+from torch.utils.data import random_split, DataLoader
+
 
 
 # Number of Patches = (img_dim - patch_size) // Stride + 1
 class PotsdamDataset(Dataset):
-    def __init__(self, image_dir_path, label_dir_path, patch_size, stride, device, transform=None):
+    COLOR_MAP = {
+        (255, 255, 255): 0,  # Impervious surfaces
+        (0, 0, 255): 1,  # Building
+        (0, 255, 255): 2,  # Low vegetation
+        (0, 255, 0): 3,  # Tree
+        (255, 0, 0): 4,  # Car
+        (255, 0, 255): 5,  # Clutter/background
+    }
+
+    def __init__(self, image_dir_path, label_dir_path, patch_size, stride, device, no_color_labels=False, transform=None):
         self.image_dir = image_dir_path
         self.label_dir = label_dir_path
         self.image_files = [path.join(image_dir_path, f) for f in sorted(os.listdir(image_dir_path)) if
@@ -20,7 +32,7 @@ class PotsdamDataset(Dataset):
                                  '.tif')]
 
         self.device = device
-
+        self.no_color_labels = no_color_labels
         self.transform = transform
 
         self.patch_size = patch_size
@@ -48,8 +60,32 @@ class PotsdamDataset(Dataset):
 
         with rasterio.open(self.image_labels[file_idx]) as img:
             label_patch = img.read(window=Window(col, row, self.patch_size, self.patch_size))
-            label_patch = torch.from_numpy(label_patch).float().to(self.device)
 
+            if self.no_color_labels:
+                label_patch = np.transpose(label_patch, (1, 2, 0))
+                class_mask = np.zeros((self.patch_size, self.patch_size), dtype=np.int64)
+
+                for color, idx in self.COLOR_MAP.items():
+                    match = np.all(label_patch == np.array(color), axis=-1)
+                    class_mask[match] = idx
+
+                # Return tensor
+                return torch.from_numpy(class_mask).long().to(self.device)
+            label_patch = torch.from_numpy(label_patch).float().to(self.device)
         # TODO: Apply Transform
 
         return image_patch, label_patch
+
+
+def get_data_loaders(dataset, dist, batch_size, seed=42):
+    train_size = int(dist[0] * len(dataset))
+    val_size = int(dist[1] * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size],
+                                                            generator=torch.Generator().manual_seed(seed))
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_loader, val_loader, test_loader

src/models.py
@@ -1,6 +1,7 @@
 from torch import nn
 from torchvision.models import resnet18, ResNet18_Weights
-
+import torchvision
+import torch
 
 def custom_resnet18(patch_size, device):
     # Load pre-trained ResNet18 model
@@ -16,3 +17,8 @@ def custom_resnet18(patch_size, device):
         params.requires_grad = True
 
     return model.to(device)
+
+
+def baseline_deeplabv3plus_resnet101(num_classes: int):
+    model = torchvision.models.segmentation.deeplabv3_resnet101(weights="DEFAULT")
+    model.classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=1)

src/train.py
@@ -1,10 +1,13 @@
-def train_loop(dataloader, model, loss_fn, optimizer, num_epochs, device):
-    num_batches = len(dataloader)
+import torch
+
+
+def train_loop(train_loader, val_loader, model, loss_fn, optimizer, num_epochs, device, save_path):
+    num_batches = len(train_loader)
     for epoch in range(num_epochs):
         model.train()
         epoch_loss = 0.0
-        for batch, (X, y) in enumerate(dataloader):
-            print(f"Batch {batch+1} / {num_batches}")
+        for batch, (X, y) in enumerate(train_loader):
+            print(f"Batch {batch + 1} / {num_batches}")
             X = X.to(device)
             y = y.to(device)
 
@@ -17,7 +20,33 @@ def train_loop(dataloader, model, loss_fn, optimizer, num_epochs, device):
             optimizer.step()
             optimizer.zero_grad()
 
-            epoch_loss += loss.item()
+            epoch_loss += loss.item() / len(X)
 
         avg_epoch_loss = epoch_loss / num_batches
-        print(f"Epoch {epoch+1} / {num_epochs}: Average loss = {avg_epoch_loss:.4f}")
+        print(f"Epoch {epoch + 1} / {num_epochs}: Average loss = {avg_epoch_loss:.4f}")
+
+        model.eval()
+        val_loss = 0.0
+
+        with torch.no_grad():
+            for batch, (X, y) in enumerate(val_loader):
+                X = X.to(device)
+                y = y.to(device)
+
+                outputs = model(X)
+
+                # Initialize batch loss for this iteration
+                batch_loss = 0.0
+                for output, y in zip(output, y):
+                    # Compute loss for each pair
+                    loss = loss_fn(outputs, y)
+                    batch_loss += loss  # Accumulate as tensor
+
+                # Average the loss over the batch (keep it as tensor)
+                val_loss += batch_loss.item() / len(X)
+
+        val_loss /= len(val_loader)
+        print(f"Validation Loss: {val_loss:.4f}")
+
+    torch.save(model.state_dict(), save_path)
+    print(f"Model saved to {save_path}!")