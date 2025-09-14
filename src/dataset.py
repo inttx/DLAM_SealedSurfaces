eff9defa51122fdb1f93a58fe52d3046475927d6
jan1na <janina.fritzke@gmail.com>
2025-09-14 21:33:37 +0200

    adjust augmentaitons

--- src/dataset.py
+++ src/dataset.py
@@ -18,13 +18,13 @@ from albumentations.pytorch import ToTensorV2
 def get_train_transform(patch_size: int):
     return A.Compose([
         A.RandomRotate90(p=0.5),
-        A.Flip(p=0.5),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=45, p=0.5),
         A.RandomBrightnessContrast(p=0.5),
         A.HueSaturationValue(p=0.5),
         A.GaussianBlur(p=0.3),
         A.GaussNoise(var_limit=(10.0, 50.0), p=0.3),
-        A.Cutout(num_holes=8, max_h_size=patch_size//8, max_w_size=patch_size//8, p=0.3),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ])
